afaec16b5dd5903c815801f4d2d7a92223e41749
Playmaker8 <playmaker8@ukr.net>
2014-11-13 22:02:21 +0200

    0.2

---
+++ pop/src/com/example/pop/Activitythree.java
@@ -0,0 +1,12 @@
+package com.example.pop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activitythree extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.three);
+	  }
+}
